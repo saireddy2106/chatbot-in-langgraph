0df87e3b990d605933dc7a0061a7483f8951fd5b
Saikumar Reddy <143153275+saireddy2106@users.noreply.github.com>
2026-06-05 21:42:56 +0530

    Implement Streamlit frontend for chatbot streaming

---
+++ streamlit_frontend_streaming.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from main import chatbot
+from langchain_core.messages import HumanMessage
+
+CONFIG = {
+    "configurable": {
+        "thread_id": "thread-1"
+    }
+}
+
+
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history']=[]
+
+#storing the message history in session state of streamlit
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+
+
+user_input = st.chat_input('Type Here')
+
+
+
+if user_input:
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    # response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]},config=CONFIG)
+    # ai_message = response['messages'][-1].content
+
+    # st.session_state['message_history'].append({'role': 'assistant','content':ai_message})
+    with st.chat_message('assistant'):
+        ai_message=st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+            config= {
+                "configurable": {
+                    "thread_id": "thread-1"
+                }
+            },
+            stream_mode='messages'
+            )
+        )
+    st.session_state['message_history'].append({'role': 'user','content':user_input})
+    st.session_state['message_history'].append({'role': 'assistant','content':ai_message})
